a010f586979558e7a07a5f4a80653aba038c129f
Cynthia <cynthia@thegracetalks.com>
2021-05-09 20:37:02 -0500

    modifying script.js file

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 var charNumb = "0123456789";
 /** uppercase variable */
 var charUpper = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
-//**lowercase variable */
+//** lowercase variable */
 var charLower = "abcdefghijklmnopqrstuvwxyz";
-//**special character variable */
+//** special character variable */
 var charSpecial = "!@#$%^&*_~`?><+=-";
 
-/** final password array value set to empty */
+// Final password array value set to empty
 var userPass = [];
 var userSelection = [];
 
@@ -19,38 +19,36 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  /**final password initial value set to empty */
+  // Final password initial value set to empty 
   userPass = [];
   
+  // Prompt for amount of characters needed for password
   var userChoice = prompt("Choose your password characters between 8 and 128 ");
-
-  /**if user choice is less than 8 or greater than 128 
-   * send an alert to please choose according to instructions 
-  * and sned the user back to first prompt */
- if(userChoice) {
-   userSelection.push([]);
- }
 
   console.log(userChoice);
 
+  // Validate amount of numbers in password
   var userChoiceNumb = confirm("Would you like to add numbers in your password?");
   if(userChoiceNumb) {
     userSelection.push(...charNumb);
   }
   console.log(userSelection);
 
+  // Validate amount of uppercase letters in password
   var userChoiceUpper = confirm("Would like to add uppercase letters in your password?")
   if(userChoiceUpper) {
     userSelection.push(...charUpper);
   }
   console.log(userSelection);
 
+  // Validate amount of lowercawse letters in password
   var userChoiceLower = confirm("Would you like to add lowercase letters in your password?")
   if(userChoiceLower) {
     userSelection.push(...charLower);
   }
   console.log (userSelection);
 
+  // Validate amount of special characters in password
   var userChoiceSpec = confirm("Would you like to add special characters to your password?")
   if(userChoiceSpec) {
     userSelection.push(...charSpecial);
@@ -60,12 +58,12 @@ function writePassword() {
   }
   console.log (userSelection);
   
-
+  // All prompts are now answered--generating password
   for (var i=0; i < userChoice; i++) {
     userPass.push(userSelection[Math.floor(Math.random() * userSelection.length)]);
     console.log(userPass.join("+"));
   }
-  //**Generate password */
+  // Generate password in document 
   document.getElementById("password").value = userPass.join("");
 
 
